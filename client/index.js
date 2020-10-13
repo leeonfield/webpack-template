90b2acb2fa41feebf48a217360a6e220588f764b
lijundong <leeonfield@foxmail.com>
2020-10-13 18:01:03 +0800

    :tada: init commit

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// import './index.css'
+ReactDOM.render(
+  <div className="container">Project Test</div>,
+  document.getElementById('root'),
+)
